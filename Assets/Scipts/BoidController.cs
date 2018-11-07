6d7e252cc2ff492bc604f7f0494f20ac9cc4496d
William <zeng.william@gmail.com>
2018-11-06 20:43:08 -0800

    added project stuff

---
+++ Assets/Scipts/BoidController.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class BoidController : MonoBehaviour {
+
+    public float speedConst = 10f;
+    public List<GameObject> boids;
+    public float rotationSpeed = 0.5f;
+    
+    private Vector3 vector;
+    private float repulsionRange = 1;
+    private float detectionRange = 3;
+    private float boundaryDetection = 1;
+    private float minX, maxX, minY, maxY;
+
+    // Use this for initialization
+    void Start () {
+        var speed = -(transform.up);
+        vector = new Vector3(speed.x, speed.y, speed.z);
+        boids = FindObjectOfType<BoidListHandler>().getBoids();
+
+        var vertExtent = Camera.main.orthographicSize;
+        var horzExtent = vertExtent * Screen.width / Screen.height;
+
+        minX = -horzExtent;
+        maxX = horzExtent;
+        minY = -vertExtent;
+        maxY = vertExtent;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        setVector();
+        checkForBoundary();
+        move();
+	}
+
+    void checkForBoundary()
+    {
+        var differenceMin = Mathf.Abs(minX - gameObject.transform.position.x);
+        var differenceMax = Mathf.Abs(maxX - gameObject.transform.position.x);
+
+        if (Mathf.Abs(differenceMin) < boundaryDetection || Mathf.Abs(differenceMax) < boundaryDetection)
+        {
+            vector.x *= -1;
+        }
+        differenceMin = Mathf.Abs(minY - gameObject.transform.position.y);
+        differenceMax = Mathf.Abs(maxY - gameObject.transform.position.y);
+
+        if (Mathf.Abs(differenceMin) < boundaryDetection || Mathf.Abs(differenceMax) < boundaryDetection)
+        {
+            vector.y *= -1;
+        }
+
+    }
+
+    void setVector()
+    {
+        Vector3 rForce = repulsiveForce();
+        Vector3 fForce = flockingForce();
+        Vector3 dForce = directionalForce();
+        //Vector3 forwardSpeed = new Vector3(transform.up.x * 0.05f, transform.up.y * 0.05f, 0);
+
+        vector = vector + rForce + fForce + dForce;
+    }
+
+    Vector3 repulsiveForce()
+    {
+        Vector3 rForce = new Vector3(0,0,0);
+        foreach(var boid in boids)
+        {
+            var difference = boid.transform.position - gameObject.transform.position;
+            var distance = getDistance(difference.x, difference.y);
+            if(Mathf.Abs(distance) < repulsionRange)
+            {
+                rForce = rForce - (boid.transform.position - transform.position);
+            }
+        }
+        return rForce;
+    }
+
+    float getDistance(float x, float y)
+    {
+        return Mathf.Sqrt(x * x + y * y);
+    }
+
+    Vector3 flockingForce()
+    {
+        Vector3 fForce = new Vector3(0, 0, 0);
+        var counter = 0;
+        foreach (var boid in boids)
+        {
+            var difference = boid.transform.position - gameObject.transform.position;
+            var distance = getDistance(difference.x, difference.y);
+            if (boid.gameObject != gameObject && Mathf.Abs(distance) < detectionRange) 
+            {
+                fForce += boid.transform.position;
+                counter++;
+            }
+        }
+        if(counter == 0)
+        {
+            return fForce;
+        }
+        fForce = fForce / counter;
+        return (fForce - transform.position) / 100;
+    }
+    
+    Vector3 directionalForce()
+    {
+        Vector3 dForce = new Vector3(0, 0, 0);
+        var counter = 0;
+        foreach (var boid in boids)
+        {
+            var difference = boid.transform.position - gameObject.transform.position;
+            var distance = getDistance(difference.x, difference.y);
+            if (boid.gameObject != gameObject && Mathf.Abs(distance) < detectionRange)
+            {
+                BoidController c = boid.GetComponent<BoidController>();
+                dForce += c.getVector();
+                counter++;
+            }
+        }
+        if(counter == 0)
+        {
+            return dForce;
+        }
+        dForce = dForce / counter;
+        return (dForce - vector) / 8;
+    }
+
+    void move()
+    {
+        transform.position = vector * speedConst + transform.position;
+    }
+
+    public Vector3 getVector()
+    {
+        return vector;
+    }
+}
---
+++ Assets/Scipts/BoidListHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class BoidListHandler : MonoBehaviour {
+    
+    public GameObject boidTemplate;
+    public List<GameObject> boids;
+
+	// Use this for initialization
+	void Start () {
+        //boids = new GameObject[maxBoids];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;
+            var pos = Camera.main.ScreenToWorldPoint(mousePos);
+            addBoid(pos.x, pos.y);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            // Casts the ray and get the first game object hit
+            if(Physics.Raycast(ray, out hit))
+            {
+                deleteBoid(hit.collider.gameObject);
+            }
+        }
+	}
+
+    public List<GameObject> getBoids()
+    {
+        return boids;
+    }
+
+    void addBoid(float x, float y)
+    {
+        var newBoid = (GameObject)Object.Instantiate(boidTemplate, new Vector3(x, y, 0), Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        boids.Add(newBoid);
+    }
+
+    void deleteBoid(GameObject boid)
+    {
+        boids.Remove(boid);
+        Destroy(boid);
+    }
+}
